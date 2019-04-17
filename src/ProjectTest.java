701b821e75ebd96d18b6defe45a60194d0959b8b
KevinEbcs <32904488+KevinEbcs@users.noreply.github.com>
2019-04-17 21:36:50 +0200

    First COmmit

--- src/ProjectTest.java
+++ src/ProjectTest.java
@@ -5,16 +5,33 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ProjectTest {
-    @Before
-    public void setUp() throws Exception {
-    }
+    Project simple_graph;
 
-    @After
-    public void tearDown() throws Exception {
+    @Before
+    public void setUp(){
+        simple_graph = new Project(
+                new String[][]{
+                        {"A", "B"}, {"C", "D"}, {"C", "E"}
+                }
+        );
     }
 
     @Test
-    public void isWellSorted() throws Exception {
+    public void test_simple_graph_1() {
+        assertTrue(simple_graph.isWellSorted(new String[]{"A","B","C","D","E"}));
     }
 
+    @Test
+    public void test_simple_graph_2() {
+        assertFalse(simple_graph.isWellSorted(new String[]{"B","A","C","D","E"}));
+    }
+
+    @Test
+    public void test_simple_graph_3() {
+        assertFalse(simple_graph.isWellSorted(new String[]{"A","B","C","D"}));
+    }
+
+    public void test_simple_graph_4() {
+        assertFalse(simple_graph.isWellSorted(new String[]{"A","A","B","C","D","E"}));
+    }
 }
